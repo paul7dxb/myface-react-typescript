a7532b8dd3af38d79f0cb4bda85228871d95f3f1
unknown <paul7markets@hotmail.com>
2023-07-25 11:28:14 +0100

    page query navigation

--- myface-frontend/src/components/PageOfPosts.tsx
+++ myface-frontend/src/components/PageOfPosts.tsx
@@ -1,16 +1,30 @@
 import { useEffect, useState } from "react";
 import Post from "./Post";
-
 import PostModel from "../models/PostModel";
+import { useSearchParams, NavLink } from "react-router-dom";
+
+import "./PageOfPosts.css"
+
+
 
 function PageOfPosts() {
 	const [posts, setPosts] = useState<PostModel[]>();
 
+	let [searchParams, setSearchParams] = useSearchParams();
+
+	const pageNumberQuery = searchParams.get('page');
+	let pageNumber:number;
+	pageNumberQuery ? pageNumber = parseInt(pageNumberQuery) : pageNumber = 1;
+
+	// function changePageHandler(newPageNumber:number){
+	// 	setSearchParams(newPageNumber);
+	// }
+
 	useEffect(() => {
-		fetch(`http://localhost:3001/posts?page=3`)
+		fetch(`http://localhost:3001/posts?page=${pageNumber}`)
 			.then((response) => response.json())
 			.then((data) => setPosts(data.results));
-	}, []);
+	}, [searchParams]);
 
 	return (
 		<>
@@ -20,6 +34,12 @@ function PageOfPosts() {
 			) : (
 				posts?.map((post) => <Post key={post.id} postData={post} />)
 			)}
+
+			{/* <button onClick={changePageHandler(pageNumber+1)}>Next</button> */}
+
+			{pageNumber>1 ? <NavLink className="post-nav-buttons" to={`/posts?page=${pageNumber-1}`} >Previous</NavLink> : undefined}
+			<NavLink className="post-nav-buttons" to={`/posts?page=${pageNumber+1}`} >Next</NavLink>
+
 		</>
 	);
 }
