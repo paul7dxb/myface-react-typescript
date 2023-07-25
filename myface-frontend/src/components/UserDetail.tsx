529870459a31518ba96ff56c4c2f485a9705fbb4
Janisha Patel <Janisha.Patel@softwire.com>
2023-07-25 14:43:19 +0100

    Added Liked & Disliked Posts

--- myface-frontend/src/components/UserDetail.tsx
+++ myface-frontend/src/components/UserDetail.tsx
@@ -8,9 +8,13 @@ import PostModel from "../models/PostModel";
 function UserDetail() {
 	let { userID } = useParams();
 
+    const [isLoading, setIsLoading] = useState(true);
+
 	const [user, setUser] = useState<UserModel>();
     const [usersPosts, setUsersPosts] = useState<PostModel[]>();
 
+    const [usersLikedPosts, setUsersLikedPosts] = useState<PostModel[]>();
+    const [usersDislikedPosts, setUsersDislikedPosts] = useState<PostModel[]>();
 
 	useEffect(() => {
 		fetch(`http://localhost:3001/users/${userID}`)
@@ -18,27 +22,33 @@ function UserDetail() {
 			.then((data) => {
 				setUser(data.user);
                 setUsersPosts(data.userPosts.results)
-                console.log(data)
+                setUsersLikedPosts(data.likedPosts.results)
+                setUsersDislikedPosts(data.dislikedPosts.results)
 			});
 	}, []);
 
+    useEffect(() => {
+        setIsLoading(user === undefined
+            || usersPosts === undefined
+            || usersLikedPosts === undefined
+            || usersDislikedPosts === undefined
+            )
+    }, [user, usersPosts, usersLikedPosts, usersDislikedPosts]);
+
 	return (
 		<div>
-
-			{user === undefined ? (
+			{isLoading ? (
 				<p>User Not Found!</p>
 			) : (
 				<div>
-					<User userData={user} />
+					<User userData={user!} />
+                    {usersPosts!.map((post) => <Post key={post.id} postData={post} />)}
+                    <h3>{user!.name}'s Liked Posts</h3>
+                    {usersLikedPosts!.map((post) => <Post key={post.id} postData={post} />)}
+                    <h3>{user!.name}'s Disliked Posts</h3>
+                    {usersDislikedPosts!.map((post) => <Post key={post.id} postData={post} />)}
 				</div>
 			)}
-
-			{usersPosts === undefined ? (
-				<p>No posts found!</p>
-			) : (
-                usersPosts.map((post) => <Post key={post.id} postData={post} />)
-			)}
-			
 		</div>
 	);
 }
