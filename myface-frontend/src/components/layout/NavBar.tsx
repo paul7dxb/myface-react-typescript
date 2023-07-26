dd89de756abcddaaf7e7525d1dca37bbd5c736bd
unknown <paul7markets@hotmail.com>
2023-07-26 15:29:37 +0100

    mobile navbar styling

--- myface-frontend/src/components/layout/NavBar.tsx
+++ myface-frontend/src/components/layout/NavBar.tsx
@@ -1,22 +1,47 @@
 import { NavLink } from "react-router-dom"
+import {useState} from "react"
 import "./NavBar.scss"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons"
+
+
 
 
 function NavBar(){
-    return <nav>
-        <NavLink   to="/"
-            className={({ isActive}) =>
-                isActive ? "navbar-active" : ""
-            } >Home</NavLink>
-        <NavLink   to="/posts"
-            className={({ isActive}) =>
-                isActive ? "navbar-active" : ""
-            } >Posts</NavLink>
-        <NavLink   to="/users"
-            className={({ isActive}) =>
-                isActive ? "navbar-active" : ""
-            } >Users</NavLink>
+
+    const [ navbarExpanded, setNavbarExpanded ] = useState(false)
+
+    const toggleNavbar = () => {
+        setNavbarExpanded(!navbarExpanded)
+    }
+
+    return (
+    <>
+    <FontAwesomeIcon  className="mobile-navbar-toggler" icon={faBars} onClick={()=>{ toggleNavbar()} } />
+
+    <nav className={`navbar ${navbarExpanded ? "navbar--expanded" : ""}`}>
+        <FontAwesomeIcon  className="mobile-navbar-toggler" icon={faXmark} onClick={ ()=>{ toggleNavbar()} } />
+
+        <h3 className="navbar__title">MyFace</h3>
+        <div className="navbar__pages">
+            <NavLink onClick={ ()=>{ toggleNavbar()} } to="/"
+                className={({ isActive}) =>
+                    isActive ? "navbar-active" : ""
+                } >Home</NavLink>
+            <NavLink onClick={ ()=>{ toggleNavbar()} }  to="/posts"
+                className={({ isActive}) =>
+                    isActive ? "navbar-active" : ""
+                } >Posts</NavLink>
+            <NavLink  onClick={ ()=>{ toggleNavbar()} } to="/users"
+                className={({ isActive}) =>
+                    isActive ? "navbar-active" : ""
+                } >Users</NavLink>
+        </div>
+        <NavLink  onClick={ ()=>{ toggleNavbar()} }  to="/posts/create"
+                className={({ isActive}) => `navbar__create-post ${isActive ? "navbar-active" : ""}` } >+ New Post</NavLink>
     </nav>
+    </>
+    )
 
 }
 
